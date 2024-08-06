e6329bab25102d42ba9f943930868750ece23592
leonardocota <leonardocsar775@gmail.com>
2024-08-06 10:14:24 -0300

    main ajustada

--- include/reversi.hpp
+++ include/reversi.hpp
@@ -1,8 +1,46 @@
 #ifndef REVERSI_HPP
 #define REVERSI_HPP
 
+#include "./jogo.hpp"
+#include "./jogador.hpp"
+#include <vector>
+#include <iostream>
+
+
 using namespace std;
 
 
+class Reversi : public jogo {
+    public:
+       vector<jogador> jogadores;
+       Reversi();
+       
+       vector<vector<char>> get_tabuleiro();
+       char get_cor_inicial();
+       vector<int> get_jogadas_possiveis_linha();
+       vector<int> get_jogadas_possiveis_coluna();
+       void imprimir_tabuleiro() override;
+       bool verificar_tabuleiro_cheio () override;
+       char encontrar_complemento_da_cor(char);
+       char seleciona_cor_aleatoria ();
+       void contagem_pontos ();
+       bool confere_vertical(int, int, char);
+       bool confere_horizontal(int, int, char);
+       bool confere_diagonal(int, int, char);
+       bool e_valido(int, int, char);
+       void jogadas_possiveis (char);
+       vector<vector<char>> inversao_da_cor_vertical(int, int, char);
+       vector<vector<char>> inversao_da_cor_horizontal(int, int, char);
+       vector<vector<char>> inversao_da_cor_diagonal(int, int, char);
+       void set_tabuleiro (int, int, char);
+       void jogar();
 
-#endif 
+    private:
+        vector<vector<char>> tabuleiro;
+        char cor_inicial;
+        vector<int> jogadas_possiveis_linha;
+        vector<int> jogadas_possiveis_coluna;
+};
+
+
+#endif
--- main.cpp
+++ main.cpp
@@ -6,60 +6,76 @@
 
 int main() {
     cadastro cadastro;
-    char letra;
+    string entrada;
 
-    cout << "Digite uma das letras: C, R, L, E ou F." << endl;
-    cout << "<string>" << endl;
+    while(true) {
+        cout << endl;
+        cout << "Digite uma das entradas:" << endl;
+        cout << "CJ ou Cadastrar Jogador" << endl;
+        cout << "RJ ou Remover Jogador" << endl;
+        cout << "LJ ou Listar Jogadores" << endl;
+        cout << "EP ou Executar Partida" << endl;
+        cout << "FS ou Finalizar Sistema" << endl;
+        cout << "<string>" << endl;
+        // A parte de digitar duas palavras ainda não está implementada (stringstream)
 
-    while(cin >> letra) {
-        switch(letra) {
-            case 'C': {
-                string apelido, nome;
-                cout << "Digite o apelido e o nome:" << endl;
-                cin >> apelido >> nome;
-                cadastro.cadastrar_jogador(apelido, nome);
-            }
+        cin >> entrada;
 
-            case 'R': {
-                string apelido;
-                cout << "Digite o apelido:" << endl;
-                cin >> apelido;
-                cadastro.remover_jogador(apelido);
-            }
+        if(entrada == "CJ" || entrada == "Cadastrar Jogador") {
+            cout << "Digite o apelido e o nome:" << endl;
+            cout << "<Apelido> <Nome>" << endl;
+            string apelido, nome;
+            cin >> apelido >> nome;
+            cadastro.cadastrar_jogador(apelido, nome);
+        }
 
-            case 'L': {
-                cadastro.listar_jogadores();
-            }
+        if(entrada == "RJ" || entrada == "Remover Jogador") {
+            cout << "Digite o apelido:" << endl;
+            cout << "<Apelido>" << endl;
+            string apelido;
+            cin >> apelido;
+            cadastro.remover_jogador(apelido);
+        }
 
-            case 'E': {
-                string apelido1, apelido2;
-                cout << "Digite os apelidos:" << endl;
-                cout << "<Apelido Jogador 1> <Apelido Jogador 2>" << endl;
-                cin >> apelido1 >> apelido2;
-                
+        if(entrada == "LJ" || entrada == "Listar Jogadores") {
+            cadastro.listar_jogadores();
+        }
+
+        if(entrada == "EP" || entrada == "Executar Partida") {
+            cout << "Digite o jogo e os apelidos dos dois jogadores:" << endl;
+            cout << "<Jogo: (R|L)> <Apelido Jogador 1> <Apelido Jogador 2>" << endl;
+            string jogo, um, dois;
+            cin >> jogo >> um >> dois;
+
+            if(jogo == "L") {
                 jogador* p1 = nullptr;
                 jogador* p2 = nullptr;
+
                 for(auto& j : cadastro.jogadores) {
-                    if (j.apelido == apelido1) {
+                    if(j.apelido == um) {
                         p1 = &j;
-                    } else if (j.apelido == apelido2) {
+                    } else if(j.apelido == dois) {
                         p2 = &j;
                     }
                 }
+
                 if(p1 == nullptr || p2 == nullptr) {
                     cout << "ERRO: Um ou ambos os jogadores não estão cadastrados." << endl;
-                    break;
+                    continue;
                 }
-                Lig4 teste(*p1, *p2);
-                teste.jogar();
 
-                break;
+                Lig4 lig4(*p1, *p2);
+                lig4.jogar();
             }
-
-            case 'F': {
-                return 0;
+            
+            if(jogo == "R") {
+                Reversi teste;
+                teste.jogar();
             }
         }
+
+        if(entrada == "FS" || entrada == "Finalizar Sistema") {
+            return 0;
+        }
     }
-    return 0;
 }
