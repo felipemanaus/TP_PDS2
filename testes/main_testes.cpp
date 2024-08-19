b2fdce3c5a94c1d53822a21b112b38f25d549fc0
leonardocota <leonardocsar775@gmail.com>
2024-08-19 20:00:19 -0300

    reversi.cpp tratado

--- testes/main_testes.cpp
+++
@@ -1,9 +0,0 @@
-#define DOCTEST_CONFIG_IMPLEMENT_WITH_MAIN
-#include "doctest.h"
-
-// Inclua aqui todos os seus arquivos de teste
-#include "teste_cadastro.cpp"
-#include "teste_jogador.cpp"
-#include "teste_jogo.cpp"
-#include "teste_lig4.cpp"
-#include "teste_reversi.cpp"
